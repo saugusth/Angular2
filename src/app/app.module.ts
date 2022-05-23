0602d84fb7f614caa52f404806680d6345e3b3b6
saugusth <54689875+saugusth@users.noreply.github.com>
2022-05-22 23:37:40 -0500

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,22 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card'
 import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { AppComponent } from './app.component';
-
-import 'hammerjs';
-
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
 import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
-
+import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
@@ -42,6 +38,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
@@ -50,7 +47,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
   ],
   providers: [
     DishService,
-    PromotionService
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -9,12 +12,20 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
   
- @Input()
- dish: Dish;
+ 
+  dish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void{
+    this.location.back();
   }
 
 }
